be2bda30c0c5ecf892e97213bfa90205b42a59a1
Simon <simon.ball.1985@gmail.com>
2017-11-01 07:24:52 +0800

    update user group id script and made part of common add usergroup script to developer image by default

---
+++ base_php7_developer/usergroup.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+USER_ID=$2
+GROUP_ID=$1
+
+# Check if the platform group exists and configure accordingly
+if grep -q "^:$GROUP_ID:" /etc/group
+    then
+         groupmod -n platform $GROUP_ID
+    else
+        if grep -q "platform" /etc/group; then
+         echo "group exists"
+       else
+         addgroup -g $GROUP_ID platform
+       fi
+    fi
+
+# Check whether the user exists and simply mod the ID if so
+if id -u php
+  then
+    usermod -u $USER_ID -g $GROUP_ID php
+  else
+    adduser -D -h /home/php -s /usr/bin/bash -G platform -u $USER_ID php
+  fi
+
+
+
