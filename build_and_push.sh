14686fb8fd7efa126cb1396526df65fb50d18c15
Simon <simon@ionic-web.com>
2020-02-13 11:59:41 +0800

    Add postgresql PHP versions

--- build_and_push.sh
+++ build_and_push.sh
@@ -1,41 +1,65 @@
 #!/bin/bash
 
-echo #################
-echo ### PHP 7 CLI ###
-echo #################
+echo '#################'
+echo '### PHP 7 CLI ###'
+echo '#################'
 cd php_7-cli \
   && docker build -t symball/php:7-cli . \
   && docker push symball/php:7-cli \
-  && cd -
+  && cd  ..
 
-echo #################
-echo ### PHP 7 DEV ###
-echo #################
+echo '#################'
+echo '### PHP 7 DEV ###'
+echo '#################'
 cd php_7-cli-dev \
   && docker build -t symball/php:7-cli-dev . \
   && docker push symball/php:7-cli-dev \
-  && cd -
+  && cd  ..
 
-echo #################
-echo ### PHP 7 SQL ###
-echo #################
+echo '#################'
+echo '### PHP 7 SQL ###'
+echo '#################'
 cd php_7-cli-sql \
   && docker build -t symball/php:7-cli-sql . \
   && docker push symball/php:7-cli-sql \
-  && cd -
+  && cd  ..
 
-echo #####################
-echo ### PHP 7 DEV/SQL ###
-echo #####################
+echo '#####################'
+echo '### PHP 7 DEV/SQL ###'
+echo '#####################'
 cd php_7-cli-dev-sql \
   && docker build -t symball/php:7-cli-dev-sql . \
   && docker push symball/php:7-cli-dev-sql \
-  && cd -
+  && cd  ..
 
-echo #####################
-echo ### PHP 7 FPM/SQL ###
-echo #####################
+echo '#####################'
+echo '### PHP 7 FPM/SQL ###'
+echo '#####################'
 cd php_7-fpm-sql \
   && docker build -t symball/php:7-fpm-sql . \
   && docker push symball/php:7-fpm-sql \
-  && cd -
+  && cd  ..
+
+echo '#######################'
+echo '### PHP 7 POSGRESQL ###'
+echo '#######################'
+cd php_7-cli-postgresql \
+  && docker build -t symball/php:7-cli-postgresql . \
+  && docker push symball/php:7-cli-postgresql \
+  && cd  ..
+
+echo '############################'
+echo '### PHP 7 DEV/POSTGRESQL ###'
+echo '############################'
+cd php_7-cli-dev-postgresql \
+  && docker build -t symball/php:7-cli-dev-postgresql . \
+  && docker push symball/php:7-cli-dev-postgresql \
+  && cd  ..
+
+echo '############################'
+echo '### PHP 7 FPM/POSTGRESQL ###'
+echo '############################'
+cd php_7-fpm-postgresql \
+  && docker build -t symball/php:7-fpm-postgresql . \
+  && docker push symball/php:7-fpm-postgresql \
+  && cd  ..
--- php_7-cli-dev/tools/update.sh
+++ php_7-cli-dev/tools/update.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-wget -O phpcbf https://squizlabs.github.io/PHP_CodeSniffer/phpcbf.phar
 wget -O phpcpd https://phar.phpunit.de/phpcpd.phar
 wget -O phpcs https://squizlabs.github.io/PHP_CodeSniffer/phpcs.phar
 wget -O phpdox http://phpdox.de/releases/phpdox.phar
@@ -8,3 +7,10 @@ wget -O phploc https://phar.phpunit.de/phploc.phar
 wget -O phpunit https://phar.phpunit.de/phpunit-7.phar
 
 chmod +x ./*
+
+echo '' > tool_versions.txt
+./phpcpd --version  >> tool_versions.txt
+./phpcs --version  >> tool_versions.txt
+./phpdox --version  >> tool_versions.txt
+./phploc --version  >> tool_versions.txt
+./phpunit --version  >> tool_versions.txt
